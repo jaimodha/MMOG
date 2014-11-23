0f760525e64c60cf80d68a10936f36934d1cf178
Mark Sargent <markesargent@gmail.com>
2014-11-23 10:43:58 -0800

    Added addBuff and setRange methods to healthbar

--- Client/UI, miniMap, chat/healthbar.py
+++ Client/UI, miniMap, chat/healthbar.py
@@ -2,24 +2,36 @@ from panda3d.core import PandaNode,NodePath,TextNode
 from panda3d.core import CardMaker
 
 class HealthBar(NodePath): 
-        def __init__(self, scale=1, value=1, r=1, g=0, b=0): 
+        def __init__(self, scale=1, value=0, r=1, g=0, b=0): 
                 NodePath.__init__(self, 'healthbar') 
-
-                self.scale = scale #right now the scale goes from 0 to 1: You can change the 
-                #scale here
-
+                self.value = value
+                self.scale = scale 
+                self.range = 1.0
+                self.buff = 0
                 cmfg = CardMaker('fg') 
                 cmfg.setFrame(- scale,  scale, -0.1 * scale, 0.1 * scale)
                 self.fg = self.attachNewNode(cmfg.generate()) 
                 self.fg.setBillboardAxis()
                 self.fg.setColor(r, g, b, 1)             
                 self.fg.setPos(1.5,0,5.8)
-                self.setValue(value) 
+                self.setValue(self.value) 
+                
+                
+              
 
         def setValue(self, value):
-                value = float(value) * 0.01
-                self.fg.setX((1-value) * self.scale * self.scale)
-                self.fg.setScale(value * self.scale, 0, self.scale) 
+                self.value = self.buff*.01 + float(value)/self.range * 0.01 
+                self.fg.setX((1-self.value) * self.scale * self.scale)
+                self.fg.setScale(self.value * self.scale, 0, self.scale) 
                 
         def reparentTo(self, parent):
             self.fg.reparentTo(parent)
+        
+        def setRange(self, value):
+            self.range *= float(value *.01)
+        
+        # assuming the buff is % of max health. Enter percents as integers
+        def addBuff(self, buffPercent):
+            self.buff = buffPercent 
+            self.setValue(self.value)
+          
