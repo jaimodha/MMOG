37b9da032eab47e34719b345aca13923aa5f2c8d
Mark Sargent <markesargent@gmail.com>
2014-11-25 09:26:09 -0800

    Added setPos method, cleaned up code

--- Client/UI, miniMap, chat/statusbar.py
+++ Client/UI, miniMap, chat/statusbar.py
@@ -1,9 +1,9 @@
 from direct.gui.DirectGui import DirectWaitBar
 #should be able to change visibility
 class ControlPointBar(DirectWaitBar):
-    def __init__(self, barColor=(255,0,0,1), pos=(1.0,0,0.9)): 
-        self.bar = DirectWaitBar(pos = pos, barColor = barColor, text="",
-        value=70, range=100, frameSize=(-0.3,0.3,0,0.03), frameColor=(0,0,255,1))
+    def __init__(self, barColor=(255,0,0,1), pos=(base.a2dRight, 0, base.a2dTop)): 
+        self.bar = DirectWaitBar(pos = pos, barColor = barColor, text="",sdfkjWDHFGP'NWELKB; 
+        value=70, range=30, frameSize=(-0.3,0.3,0,0.03), frameColor=(0,0,255,1))
 
     def setValue(self, value):
         self.bar['value'] = float(value)
@@ -13,12 +13,15 @@ class ControlPointBar(DirectWaitBar):
     
     def show(self):
         self.bar.show()
+    
+    def setPos(pos):
+        self.bar['pos'] = pos
         
     
 class ResourceBar(DirectWaitBar):
     def __init__(self, barColor=(200,180,0,1), pos=(0,0,0.9)): 
         self.bar = DirectWaitBar(pos = pos, barColor = barColor, text="",
-        value=70, range=100, frameSize=(-0.3,0.3,0,0.03), frameColor=(0,0,0,1))
+        value=70, range=30, frameSize=(-0.3,0.3,0,0.03), frameColor=(0,0,0,1))
 
     def setValue(self, value):
         self.bar['value'] = float(value)
