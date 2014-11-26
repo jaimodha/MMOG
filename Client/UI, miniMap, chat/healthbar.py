663e2fd50fd57148524c1509ad22eb1d583441b5
Mark Sargent <markesargent@gmail.com>
2014-11-25 17:57:50 -0800

    Background added to healthbar, flickering shading fixed, other bug fixes.

--- Client/UI, miniMap, chat/healthbar.py
+++ Client/UI, miniMap, chat/healthbar.py
@@ -2,30 +2,49 @@ from panda3d.core import PandaNode,NodePath,TextNode
 from panda3d.core import CardMaker
 
 class HealthBar(NodePath): 
-        def __init__(self, scale=1, value=0, r=1, g=0, b=0): 
+        def __init__(self, scale=1, value=0, r=10, g=0, b=0): 
                 NodePath.__init__(self, 'healthbar') 
                 self.value = value
                 self.scale = scale 
                 self.range = 1.0
                 self.buff = 0
+                
+                
+                
+                cmbg = CardMaker('bg') 
+                cmbg.setFrame(- scale, scale, -0.1 * scale, 0.1 * scale) 
+                self.bg = self.attachNewNode(cmbg.generate())
+ 
+                self.bg.setColor(0.2, 0.2, 0.2, 1) 
+                self.bg.setPos(0,0,5.8)
+                
+                
                 cmfg = CardMaker('fg') 
                 cmfg.setFrame(- scale,  scale, -0.1 * scale, 0.1 * scale)
-                self.fg = self.attachNewNode(cmfg.generate()) 
-                self.fg.setBillboardAxis()
+                self.fg = self.bg.attachNewNode(cmfg.generate()) 
+               
                 self.fg.setColor(r, g, b, 1)             
-                self.fg.setPos(1.5,0,5.8)
-                self.setValue(self.value) 
+                self.fg.setPos(0,-0.1,0)
+                self.fg.setBillboardPointWorld()
+                self.bg.setBillboardPointWorld()
                 
-                
+                self.fg.clearShader()
+                self.bg.clearShader()
+       
+
+                self.setValue(0)
               
 
         def setValue(self, value):
                 self.value = self.buff*.01 + float(value)/self.range * 0.01 
-                self.fg.setX((1-self.value) * self.scale * self.scale)
                 self.fg.setScale(self.value * self.scale, 0, self.scale) 
+                self.fg.setX((self.value - 1) * self.scale * self.scale)
+
                 
         def reparentTo(self, parent):
-            self.fg.reparentTo(parent)
+            self.bg.reparentTo(parent)
+            self.fg.reparentTo(self.bg)
+            
         
         def setRange(self, value):
             self.range *= float(value *.01)
