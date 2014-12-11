0a7dac2347cb089d8524b605c3a30cca6717d1e1
dsyed <daanyaalasyed@gmail.com>
2014-12-10 23:14:36 -0800

    Removed Minimap references from ControlPointClient

--- CharacterMovementWithNetwork/ControlPointClient/ControlPointClient.py
+++ CharacterMovementWithNetwork/ControlPointClient/ControlPointClient.py
@@ -8,8 +8,6 @@ import random, sys, os, math
 
 from BasicControlPoint import *
 
-from miniMap import *
-
 from common.Constants import Constants
 from net.ConnectionManager import ConnectionManager
 import __builtin__
@@ -69,8 +67,6 @@ class World(DirectObject):
                     if cp.timer == 0:
                         print("CP [", cp.id, "] taken by Blue")
                         cp.factionId = BLUE
-                        main.miniMap.changeTowerColor("tower"+cp.id, '/CharacterClient/models/tower_blue.png')
-                        #main.controlNpc.switchControl(cp.id)
                         self.cManager.sendRequest(Constants.CMSG_NPCDEATH, [cp.id])
                         
                 else:
@@ -86,8 +82,6 @@ class World(DirectObject):
                     if cp.timer == 30:
                         print("CP [", cp.id, "] taken by Red")
                         cp.factionId = RED
-                        main.miniMap.changeTowerColor("tower"+cp.id, '/CharacterClient/models/tower_red.png')
-                        #main.controlNpc.switchControl(cp.id)
                 else:
                     cp.timer = 0
             
