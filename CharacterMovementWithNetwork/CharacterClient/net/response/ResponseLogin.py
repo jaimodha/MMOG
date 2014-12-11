56b48129a04e99d4a4bb822980e5e01ccb8050d1
burgui <burgui@users.noreply.github.com>
2014-12-10 22:07:04 -0800

    Update ResponseLogin.py

--- CharacterMovementWithNetwork/CharacterClient/net/response/ResponseLogin.py
+++ CharacterMovementWithNetwork/CharacterClient/net/response/ResponseLogin.py
@@ -43,11 +43,11 @@ class ResponseLogin(ServerResponse):
                         main.base.camera.setPos(main.player._character.getX(),main.player._character.getY()+10,2)
 
                         main.miniMap = miniMap(main.player._character)
-                        main.tower1 = main.miniMap.setTower("tower1", '/CharacterClient/models/tower_red.png', 0.08, 210.984, 115.005)
-                        main.tower2 = main.miniMap.setTower("tower2", '/CharacterClient/models/tower_red.png', 0.08, 141.016, 0.440607)
-                        main.tower3 = main.miniMap.setTower("tower3", '/CharacterClient/models/tower_red.png', 0.08, -0.903916, 11.3765)
-                        main.tower4 = main.miniMap.setTower("tower4", '/CharacterClient/models/tower_blue.png', 0.08, -210.771, 113.753)
-                        main.tower5 = main.miniMap.setTower("tower5", '/CharacterClient/models/tower_blue.png', 0.08, -149.953, 0.674369)
+                        main.tower1 = main.miniMap.setTower("tower1", '//CharacterClient/models/tower_red.png', 0.08, 210.984, 115.005)
+                        main.tower2 = main.miniMap.setTower("tower2", '//CharacterClient/models/tower_red.png', 0.08, 141.016, 0.440607)
+                        main.tower3 = main.miniMap.setTower("tower3", '//CharacterClient/models/tower_red.png', 0.08, -0.903916, 11.3765)
+                        main.tower4 = main.miniMap.setTower("tower4", '//CharacterClient/models/tower_blue.png', 0.08, -210.771, 113.753)
+                        main.tower5 = main.miniMap.setTower("tower5", '//CharacterClient/models/tower_blue.png', 0.08, -149.953, 0.674369)
                         
             else:
                         #swordsmanStartPos = main.environ.find("**/start_point").getPos()
